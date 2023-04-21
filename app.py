3779fa2122a3ca10914eab4a6ed291294637a90d
newahmedyehia <124242960+newahmedyehia@users.noreply.github.com>
2023-04-21 21:16:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-app = FastAPI()
-@app.get("/")
-async def index(request: Request):
-    return "Welcome to Golab REST API"
+from flask import Flask,request
+app = Flask(__name__)
 
-@app.get("/data")
-async def get_data(request: Request):
-    idea = request.query_params.get('idea')
-    if idea:
-        data = {'idea': idea}
-        return JSONResponse(content=data)
-    else:
-        return "Please provide an idea parameter in the URL."
+@app.route("/")
+def home():
+	return "HELLO from vercel use flask"
+
+@app.route('/example', methods=['POST'])
+def example():
+    if request.method == 'POST':
+        data = request.form['data']
+        print(data)
+        return f"The data you sent is: {data}"
+
+@app.route("/about")
+def about():
+	return "HELLO about"
